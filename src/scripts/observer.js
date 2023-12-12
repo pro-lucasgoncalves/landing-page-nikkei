9d48db3cd90829836eb8940d812de5eb95c5c613
Lucas Rocha Gonçalves <pro.lucasgoncalves@gmail.com>
2023-12-12 15:12:42 -0400

    animacoes

--- src/scripts/observer.js
+++ src/scripts/observer.js
@@ -1,12 +1,14 @@
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('fade-in-image');
-      }
-    });
+const observer = new IntersectionObserver((entries) => {
+  // Loop over the entries
+  entries.forEach((entry) => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add("fade-in-image");
+    }
   });
-  
-  observer.observe(document.querySelector('.clientes'));
+});
+
+observer.observe(document.querySelector(".clientes"));
+observer.observe(document.querySelector(".three-columns .row"));
+squares.forEach((element) => observer.observe(element));
